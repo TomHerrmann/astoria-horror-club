1404d7e8015f1e69a546b5dd915f0818a590586c
Tom Herrmann <tomherrmanndev@gmail.com>
2025-06-03 21:48:31 -0400

    remove newletter form / add logo

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fzuxxxhgqwm9izz9.public.blob.vercel-storage.com',
+        pathname: '**',
+      },
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './globals.css'
 import SubscriberForm from './components/SubscriberForm'
+import Image from 'next/image'
 
 /**
  *
@@ -10,7 +11,12 @@ export default async function HomePage() {
   return (
     <div className="home">
       <div className="content">
-        <SubscriberForm />
+        <Image
+          src="https://fzuxxxhgqwm9izz9.public.blob.vercel-storage.com/astoria_horror_club_logo_COMPRESSED-MLhBXHfP0iqlcMqxilZo5opRscHZRd.png"
+          alt="astoria horror club logo"
+          width={500}
+          height={500}
+        />
       </div>
     </div>
   )
